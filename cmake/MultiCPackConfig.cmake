36b42f51482faa84f4c3edfd9ad7912721f10bd8
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-19 14:44:20 -0400

    [.gitignore] Ignore build dir and IDEA; [src/CMakeLists.txt] Set library version correctly; [cmake/MultiCPackConfig.cmake] Rename project

--- cmake/MultiCPackConfig.cmake
+++ cmake/MultiCPackConfig.cmake
@@ -1,6 +1,6 @@
 include("release/CPackConfig.cmake")
 
 set(CPACK_INSTALL_CMAKE_PROJECTS
-        "debug;tree_sitter;ALL;/"
-        "release;tree_sitter;ALL;/"
+        "debug;tree_sitter_c;ALL;/"
+        "release;tree_sitter_c;ALL;/"
 )
