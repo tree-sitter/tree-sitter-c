c909de991934d07e9bb243c7acab3770e3524576
Chinmay Dalal <~chinmay/public-inbox@lists.sr.ht>
2026-04-19 14:07:20 -0400

    update zig to 0.16

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     zig-overlay = {
       url = "github:mitchellh/zig-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
     zls = {
-      url = "github:zigtools/zls";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:zigtools/zls/0.16.0";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -21,7 +21,7 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      zig-version = "0.15.1";
+      zig-version = "0.16.0";
       zig = zig-overlay.packages.${system}.${zig-version};
     in
     {
